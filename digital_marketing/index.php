25e83f5a8ca42a2ad017c5f5c3fe22f29c481a55
chetat <yekuwilfred@gmail.com>
2020-07-09 20:44:37 +0100

    Finish Digital Marketing page

--- digital_marketing/index.php
+++ digital_marketing/index.php
@@ -1,7 +1,127 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/assets/html/header.php'); ?>
+<header class="hero">
 
-<div class="section30">
-	<h1>Page under construction</h1>
-</div>
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="hero-text col-12 text-center">
+                <h1>We offer Digital Marketing Services Content</h1>
+                <p class="lead">Best services you will get</p>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="about-service">
+    <div class="section-title">
+        <h2 class="text-center mb-5">Digital Marketing Services</h2>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-6 pt-4">
+                <h3>Voluptatem dignissimos provident quasi corporis</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore
+                    magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore
+                    magna aliqua.
+                </p>
+                <p>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                </p>
+            </div>
+            <div class="col-lg-6 col-md-6 pt-4">
+                <img src="/assets/img/digital_m/intro-img.svg" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cta-section">
+    <div class="section-title">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-8">
+                    <h2 class="text-center">Approach to enhance <br>
+                        the branding experience</h2>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="description-section">
+    <div class="section-title">
+        <h2 class="text-center mb-5">What We offer</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+		<div class="col-lg-4 col-md-4">
+<img src="/assets/img/digital_m/features.svg" alt="">
+			</div>
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 mt-2 d-flex">
+                        <div class="card flex-fill">
+                            <div class="card-body">
+                                <h4 class="card-title text-center">Social Media Marketing</h4>
+                                <p class="card-text">
+								is simply dummy text of the printing and typesetting industry. 
+								Lorem Ipsum has been the industry's standard dummy tex
+								t ever since the 1500s, when an unknown printer took a galley of 
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 mt-2 d-flex">
+                        <div class="card flex-fill">
+                            <div class="card-body">
+                                <h4 class="card-title text-center">Search Engine Optimization</h4>
+                                <p class="card-text">
+								is simply dummy text of the printing and typesetting industry. 
+								Lorem Ipsum has been the industry's standard dummy tex
+								t ever since the 1500s, when an unknown printer took a galley of 
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 mt-2 d-flex">
+                        <div class="card flex-fill">
+                            <div class="card-body">
+                                <h4 class="card-title text-center">Email marketing</h4>
+                                <p class="card-text">
+								is simply dummy text of the printing and typesetting industry. 
+								Lorem Ipsum has been the industry's standard dummy tex
+								t ever since the 1500s, when an unknown printer took a galley of 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 mt-2 d-flex">
+                        <div class="card flex-fill">
+                            <div class="card-body">
+                                <h4 class="card-title text-center">Infographics and Ads</h4>
+                                <p class="card-text">
+								is simply dummy text of the printing and typesetting industry. 
+								Lorem Ipsum has been the industry's standard dummy tex
+								t ever since the 1500s, when an unknown printer took a galley of 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+			</div>
+			
+        </div>
+    </div>
+    </div>
+</section>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/assets/html/footer.php'); ?>
